00085096c9c6511851c4da7335d3b25c300f0de4
matt <vaporlinx@gmail.com>
2018-03-07 19:42:31 -0700

    get roster working

--- src/pages/roster.js
+++ src/pages/roster.js
@@ -3,6 +3,17 @@ import * as urlHelper from "../../src/urlHelper.js";
 const template = document.createElement("template");
 template.innerHTML = `
     <style>
+        #roster {
+            display: flex;
+            width: 100vw;
+            height: 100vh;
+            overflow: auto;
+            flex-wrap: wrap;
+        }
+
+        #roster > unit-card {
+            margin: 5px;
+        }
     </style>
 
     <div id="roster"> </div>
@@ -24,32 +35,37 @@ export default class rosterPage extends HTMLElement {
         this.rosterElem = this.shadowRoot.getElementById("roster");
 
         window.addEventListener("urlUpdated", async event => {
-            let unitIds = [142];
-            if (urlHelper.getParams().unitIds) {
-                unitIds = urlHelper.consumeParams(["unitIds"]).unitIds.split(",").map(id => parseInt(id));
-            }
-            this.buildRoster(unitIds);
+            this.buildRoster(this.getIdsFromUrl());
         });
+        this.buildRoster(this.getIdsFromUrl());
+    }
+
+    getIdsFromUrl() {
+        if (urlHelper.getParams().unitIds) {
+            return urlHelper.consumeParams(["unitIds"]).unitIds.split(",").map(id => parseInt(id))
+        }
+        return [];
     }
 
     async buildRoster(ids) {
         while (this.rosterElem.hasChildNodes()) {
             this.rosterElem.removeChild(this.rosterElem.lastChild);
         }
-        try {
-            const unParsed = await window.fetch(`/sw-units?unitIds=${ids.join(",")}`);
-            const data = JSON.parse(await unParsed.text());
-            for (const unit of data) {
-                const card = document.createElement("unit-card");
-                card.data = unit;
-                this.rosterElem.appendChild(card);
+        if (ids.length) {
+            try {
+                const unParsed = await window.fetch(`/sw-units?unitIds=${ids.join(",")}`);
+                const data = JSON.parse(await unParsed.text());
+                for (const unit of data) {
+                    const card = document.createElement("unit-card");
+                    card.data = unit;
+                    this.rosterElem.appendChild(card);
+                }
             }
-        }
-        catch (err) {
-            global.handleError(`Error getting unit: ${err}`);
-        }
+            catch (err) {
+                global.handleError(`Error getting unit: ${err}`);
+            }
+        } 
     }
-
 }
 
 customElements.define("roster-page", rosterPage);
